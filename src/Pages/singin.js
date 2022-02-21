23505ba6606bc3947c38f5658faa98add69e8c9e
huonghtph13873 <huonghtph13873@fpt.edu.vn>
2022-02-22 01:35:06 +0700

    update

--- src/Pages/singin.js
+++ src/Pages/singin.js
@@ -22,7 +22,7 @@ const Singin = {
             placeholder="Email address">
         </div>
         <div>
-          <label for="password" class="sr-only"></label>
+          <label for="password" class="sr-only">Mật khẩu : </label>
           <input id="password" name="password" type="password" autocomplete="current-password" required
             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Password">
