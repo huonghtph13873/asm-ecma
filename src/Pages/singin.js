3fce11fd7e9d7b1fff2b887ed3bfae8aeb253be9
huonghtph13873 <huonghtph13873@fpt.edu.vn>
2022-02-22 01:41:16 +0700

    update

--- src/Pages/singin.js
+++ src/Pages/singin.js
@@ -10,7 +10,7 @@ const Singin = {
     <div>
       <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
         alt="Workflow">
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Đăng nhập vào tài khoản</h2>
     </div>
     <form class="mt-8 space-y-6" method="POST" id="formSignin">
       <input type="hidden" name="remember" value="true">
