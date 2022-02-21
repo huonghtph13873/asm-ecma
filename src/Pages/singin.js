945fd700ec8a809872137e644474e8637e4bfb24
huonghtph13873 <huonghtph13873@fpt.edu.vn>
2022-02-22 01:34:44 +0700

    update

--- src/Pages/singin.js
+++ src/Pages/singin.js
@@ -16,13 +16,13 @@ const Singin = {
       <input type="hidden" name="remember" value="true">
       <div class="rounded-md shadow-sm -space-y-px">
         <div>
-          <label for="email-address" class="sr-only">Email address</label>
+          <label for="email-address" class="sr-only">Địa chỉ email : </label>
           <input id="email-address" name="email" type="email" autocomplete="email" required
             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Email address">
         </div>
         <div>
-          <label for="password" class="sr-only">Password</label>
+          <label for="password" class="sr-only"></label>
           <input id="password" name="password" type="password" autocomplete="current-password" required
             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Password">
